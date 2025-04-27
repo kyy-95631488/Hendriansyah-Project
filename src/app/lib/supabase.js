a01f10e26a2742a7ed140c5f1bac6b138ed64ec8
kyy-95631488 <riskir5622@gmail.com>
2025-04-27 19:57:56 +0700

    Update Commit [ 27 - 04 - 2025 | 19:57 ]

--- src/app/lib/supabase.js
+++ src/app/lib/supabase.js
@@ -1,5 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl = 'https://cbpzbbjczpxfwmffalbf.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNicHpiYmpjenB4ZndtZmZhbGJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU3MzI1ODcsImV4cCI6MjA2MTMwODU4N30.7eIxWePocxFo39NpNABuIE0BDuNJNMYqIxELJtxIFu4';
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+// import { createClient } from '@supabase/supabase-js';
+
+// const supabaseUrl = process.env.SUPABASE_URL;
+// const supabaseKey = process.env.SUPABASE_KEY;
+// export const supabase = createClient(supabaseUrl, supabaseKey);
